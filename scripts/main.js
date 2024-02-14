f58d09a7d2313a4238b37c5308d18a78a3838a18
levimaciell <levimaciell1221@gmail.com>
2024-02-14 16:32:01 -0300

    Creating new command blocks when clicking enter

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 const command_line = document.getElementById("input_field");
-
-console.log(command_line);
+const main_content = document.getElementById("main__content");
+const command_block = document.getElementById("command__block");
 
 command_line.addEventListener("keypress", function(e){
     if(e.key === "Enter"){
         command_line.value = "";
         console.log("Enter was pressed");
+        const copycat = command_block.cloneNode(true);
+        main_content.appendChild(copycat);
     }
 });
 
