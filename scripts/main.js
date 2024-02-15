0276d2b29b56705ab1da04513a5fda50235e7766
levimaciell <levimaciell1221@gmail.com>
2024-02-15 10:55:12 -0300

    Fixing bug into terminal linearity

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,30 @@
-const command_line = document.getElementById("input_field");
-const main_content = document.getElementById("main__content");
-const command_block = document.getElementById("command__block");
+const main_content = document.getElementById("main__block");
+let command_block = document.getElementById("command__block");
+let command_line = command_block.childNodes[3].childNodes[3];
 
-command_line.addEventListener("keypress", function(e){
+command_line.addEventListener("keypress", handleEnterPress);
+
+function handleEnterPress(e){
     if(e.key === "Enter"){
-        command_line.value = "";
-        console.log("Enter was pressed");
-        const copycat = command_block.cloneNode(true);
-        main_content.appendChild(copycat);
-    }
-});
+        /*Creating copy of div and adding*/
+        const clone = command_block.cloneNode(true);
+        main_content.appendChild(clone);
 
+        /*Adding and removing the event listener*/
+        cleanEventListenerAndInput(clone);
+    }
+}
+
+function cleanEventListenerAndInput(clonedBlock){
+    const cloneInput = clonedBlock.childNodes[3].childNodes[3];
+    command_line.removeEventListener('keypress', arguments.callee);
+    cloneInput.addEventListener("keypress", handleEnterPress);
+    cleanInput(cloneInput);
+}
+
+function cleanInput(cloneInput){
+    cloneInput.value = "";
+    command_line.disabled = true;
+    cloneInput.disabled = false;
+    command_line = cloneInput;
+}
