13cc1846f1f2a45efad07af99c10bc4e2d227435
levimaciell <levimaciell1221@gmail.com>
2024-03-25 13:05:06 -0300

    finishing responsive navbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
-console.log("Olá do index js");
+botao = document.getElementById('botao');
+conteudo = document.getElementById('links');
+
+document.addEventListener('DOMContentLoaded', function(){
+
+
+    botao.addEventListener('click', function(event){
+        event.preventDefault();
+
+        if(conteudo.style.display === 'block'){
+            conteudo.style.display = 'none';
+        }
+        else{
+            conteudo.style.display = 'block';
+        }
+    });
+
+    if(window.innerWidth > 500){
+        conteudo.style.display = 'flex';
+    }
+});
+
